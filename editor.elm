e4cb56daaefd25a834c33b56ebcd3e5d8ec7c358
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-12-31 14:39:18 -0700

    clean up

--- editor.elm
+++ editor.elm
@@ -8,39 +8,13 @@ import Json.Decode as Json
 
 ---------- MODEL -------------
 
---type alias Model = 
---    { input: String
---    , render: String
---    }
-
 type alias Model = String
 
-
 init : Model
---init = 
---    { input = "<div>Hello elm</div>"
---    , render = "<div>Hello elm</div>"
---    }
 init = ""
 
 ---------- UPDATE -------------
 
---type Action = 
---        UpdateIFrame |
---        UpdateInput
-
-
---update : Action -> Model -> String -> Model 
---update action model str = 
---    case action of
---        UpdateIFrame ->
---            { model |
---                render = model.input
---            }
---        UpdateInput ->
---            { model |
---                input = str
---            }
 update : Model -> Model -> Model
 update newStr oldStr = 
     newStr
@@ -53,14 +27,6 @@ view address model =
     div 
         []
         [makeTitle, mainView address model]
-
-
---makeButton : Signal.Address Model -> Html
---makeButton address = 
---    button 
---        [ onClick address UpdateIFrame, ""
---        , style [("display", "block")] ] 
---        [ text "Render" ]
 
 
 makeInput : Signal.Address Model -> Model -> Html
@@ -77,16 +43,14 @@ makeTitle =
         [ style [("font-size", "2em")]]
         [ text "W3-Elm"]
 
--- first arg is an address that expects an Action type
+-- first arg is an address that expects an Model type
 mainView : Signal.Address Model -> Model -> Html
 mainView address model = 
     let editor : Html
         editor = 
             div 
                 [ style [("width", "50%")] ] 
-                [ 
-                makeInput address model
-                ]
+                [ makeInput address model ]
         render : Html
         render = 
             -- render the model in the iframe, the model is saved when the button is pressed
